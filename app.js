3b1da1a9db37335329f27f2216e793f7a92a4bf2
Duncan H <duncan.haran@gmail.com>
2020-02-15 18:04:44 -0500

    moved JS to HTML for readability, display attributes added to css

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
+let ans = "";
 const answerGenerator = () => {
-  let randNum = math.random(3);
-  let ans = "";
+  let randNum = Math.floor(Math.random() * Math.floor(3));
   document.getElementById(`button-holder`).style.display = "block";
   switch (randNum) {
-    case randNum <= 1:
+    case 0:
       ans = "rock";
       break;
-    case randNum >= 2:
-      ans = "scissors";
-      break;
-    default:
+    case 1:
       ans = "paper";
+      break;
+    case 2:
+      ans = "scissors";
       break;
   }
   return ans;
 };
-
 const answerCheck = (ans, type) => {
   if (ans == type) {
     document.getElementById(`win`).style.display = "block";
+  } else {
+    document.getElementById(`loss`).style.display = "block";
   }
-  document.getElementById(`loss`).style.display = "block";
 };
-
-export { answerCheck, answerGenerator };
+export { answerCheck, answerGenerator, ans };
